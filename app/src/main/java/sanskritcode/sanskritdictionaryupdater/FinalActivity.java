88dd0c4f6a2c8d98b777ccf378d8405e9fd8b62d
vishvAsaH <vishvas.vasuki@gmail.com>
2018-02-02 17:44:14 -0800

    Add better links

--- app/src/main/java/sanskritcode/sanskritdictionaryupdater/FinalActivity.java
+++ app/src/main/java/sanskritcode/sanskritdictionaryupdater/FinalActivity.java
@@ -3,6 +3,7 @@ package sanskritcode.sanskritdictionaryupdater;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +25,7 @@ public class FinalActivity extends BaseActivity {
         }
         setContentView(R.layout.activity_final);
         TextView topText = findViewById(R.id.final_act_textView);
+        topText.setMovementMethod(LinkMovementMethod.getInstance());
         topText.setText(getString(R.string.finalMessage));
         largeLog(getLocalClassName(), dictIndexStore.toString());
         final StringBuilder failures = new StringBuilder("");
