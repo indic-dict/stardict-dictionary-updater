0d5e5aeb60402ec62341c0f909d37213188d670c
vvasuki <vishvas.vasuki@gmail.com>
2019-02-07 11:07:27 -0800

    Move code to java src directory - less confusion for android studio, hopefully.

--- downloaderFlow/src/test/java/DictionaryGetterTest.kt
+++ downloaderFlow/src/test/java/DictionaryGetterTest.kt
@@ -14,6 +14,7 @@ class DictionaryGetterTest {
     fun test_playground() {
         try {
             val url = "https://github.com/sanskrit-coders/stardict-sanskrit/releases/download/2017-04-14/Meulenbeld-Sanskrit-Names-of-Plants__2017-04-17_08-29-39__0MB.tar.gz"
+            @Suppress("UnstableApiUsage")
             logger.info(
                     Files.getNameWithoutExtension(url))
             val fileName = url.substring(url.lastIndexOf("/")).replace("/", "")
